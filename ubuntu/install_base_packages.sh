ce361df7607cd62cf9083c7fd9488edcf2c66ec1
John.Grayson <cgg5207@sina.com>
2013-04-09 22:19:20 +0800

    解决: Searching for binary rubies, this might take some time. Installing requirements for ubuntu, might require sudo password. Skipping `apt-get update` make sure your system is up to date. RVM autolibs is now configured with mode '2' => 'check and stop if missing', please run `rvm autolibs enable` to let RVM do its job or run and read `rvm autolibs [help]` or visit https://rvm.io/rvm/autolibs for more information.

--- ubuntu/install_base_packages.sh
+++ ubuntu/install_base_packages.sh
@@ -1,6 +1,6 @@
 echo "install Ubuntu base packages..."
 sudo apt-get update
-sudo apt-get install -y wget vim build-essential openssl libreadline6 libreadline6-dev libmysqlclient-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf automake libtool imagemagick libmagickwand-dev libpcre3-dev 
+sudo apt-get install -y wget vim build-essential openssl libreadline6 libreadline6-dev libmysqlclient-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf automake libtool imagemagick libmagickwand-dev libpcre3-dev libsqlite3-dev sqlite3 libgdbm-dev libncurses5-dev bison libffi-dev 
 echo "---------------------------------------------------------------------------"
 echo ""
 
